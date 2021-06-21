b94950e3120fcd25dc53774c3e6a6f255d571758
GuilhermeSR98 <guigareisbr@gmail.com>
2021-06-21 15:20:32 -0300

    Creating methods

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
 class Client{
     name;
     cpf;
-    agency;
-    balance;
     rg
+}
+
+class CurrentAcount{
+     agency = 0001;
+     balance = 0;
+     withdraw(value) {
+        if(this.balance >= value && value >= 0) {
+            this.balance -= value;
+        }
+        else {
+            console.log(`Insufficient balance to do a ${value} withdraw.`);
+        }
+    }
+    deposit(value){
+        if(value > 0) {
+          this.balance += value;  
+        }
+        else {
+            console.log('Must be a value greater than 0.')
+        }
+    }
 }
 
 const client1 = new Client();
 
 const client2 = new Client();
 
+const currentAcount1 = new CurrentAcount();
+
+const currentAcount2 = new CurrentAcount(); 
+
 client1.name = 'Guilherme';
 client1.cpf = '028.544.021-90';
-client1.agency = 'St. Leste';
-client1.balance = 2.000;
 client1.rg = 5384207;
 
 client2.name = 'Eduarda';
 client2.cpf = '312.014.021-52';
-client2.agency = 'St. Oeste';
-client2.balance = 6.000;
 client2.rg = 2309100;
+
+currentAcount1.agency = 'St. Leste';
+currentAcount1.deposit(2000);
+console.log(`${client1.name}'s balance: ${currentAcount1.balance}`);
+
+
+currentAcount2.agency = 'St. Oeste';
+currentAcount2.deposit(6000);
+console.log(`${client2.name}'s balance: ${currentAcount2.balance}`);
+
+currentAcount1.withdraw(500);
+currentAcount2.withdraw(1000);
+
+console.log(client1,  currentAcount1, '\n', client2, currentAcount2);
