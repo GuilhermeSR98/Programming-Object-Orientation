3477f3b448b9520784a68863d56b0a6da5b22f93
GuilhermeSR98 <guigareisbr@gmail.com>
2021-06-21 18:01:40 -0300

    Class composition

--- Client.js
+++ Client.js
@@ -1,13 +1,23 @@
+export class Client{
+    name;
+    cpf;
+    rg
+}
+
 export class CurrentAcount{
     agency = 1;
     balance = 0;
+    client;
+
     withdraw(value) {
-       if(this.balance <= value && value <= 0) {
+       if(this.balance < value || value <= 0) {
            console.log(`Insufficient balance to do a ${value} withdraw.`);
            return;
        }
        this.balance -= value;
+       return value;
    }
+
    deposit(value){
        if(value > 0) {
          this.balance += value;  
@@ -16,11 +26,13 @@ export class CurrentAcount{
            console.log('Must be a value greater than 0.')
        }
    }
-}
 
-export class Client{
-    name;
-    cpf;
-    rg
+   transfer(value, acount){
+       if(value <= 0 || value > this.balance){
+           console.log(`Unable to transfer.`);
+           return;
+       }
+       const amount = this.withdraw(value);
+       acount.deposit(amount);
+   }
 }
-
--- index.js
+++ index.js
@@ -19,15 +19,20 @@ client2.cpf = '312.014.021-52';
 client2.rg = 2309100;
 
 currentAcount1.agency = 'St. Leste';
+currentAcount1.client = client1;
 currentAcount1.deposit(2000);
 console.log(`${client1.name}'s balance: ${currentAcount1.balance}`);
 
 
 currentAcount2.agency = 'St. Oeste';
+currentAcount2.client = client2;
 currentAcount2.deposit(6000);
 console.log(`${client2.name}'s balance: ${currentAcount2.balance}`);
 
-currentAcount1.withdraw(500);
+currentAcount1.withdraw(1000);
 currentAcount2.withdraw(1000);
 
-console.log(client1,  currentAcount1, '\n', client2, currentAcount2);
+currentAcount1.transfer(1000, currentAcount2);
+
+console.log(currentAcount1, '\n',currentAcount2);
+
