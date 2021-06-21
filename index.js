7d2fbfb7c093f55e2adc3b418ae0f0efe621fb7b
GuilhermeSR98 <guigareisbr@gmail.com>
2021-06-21 09:45:14 -0300

    Creating classes and objects

---
+++ index.js
@@ -0,0 +1,23 @@
+class Client{
+    name;
+    cpf;
+    agency;
+    balance;
+    rg
+}
+
+const client1 = new Client();
+
+const client2 = new Client();
+
+client1.name = 'Guilherme';
+client1.cpf = '028.544.021-90';
+client1.agency = 'St. Leste';
+client1.balance = 2.000;
+client1.rg = 5384207;
+
+client2.name = 'Eduarda';
+client2.cpf = '312.014.021-52';
+client2.agency = 'St. Oeste';
+client2.balance = 6.000;
+client2.rg = 2309100;
