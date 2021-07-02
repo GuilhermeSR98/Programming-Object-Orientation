636b3d7ace69b4fc61bfb3ac6c80ffc084a6c18c
GuilhermeSR98 <guigareisbr@gmail.com>
2021-07-02 20:40:23 -0300

    Settings

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-import { CurrentAcount } from "./Client.js";
+import { CurrentAcount } from "./CurrentAcount.js";
 import { Client } from "./Client.js";
 
 
 const client1 = new Client('Guilherme', '028.544.021-90', 5384207);
-
 const client2 = new Client('Eduarda', '312.014.021-52', 2309100);
 
 const currentAcount1 = new CurrentAcount('St. Leste', client1);
-
 const currentAcount2 = new CurrentAcount('St. Oeste', client2); 
 
 currentAcount1.deposit(2000);
-console.log(`${client1.name}'s balance: ${currentAcount1.balance}`);
-
 currentAcount2.deposit(6000);
-console.log(`${client2.name}'s balance: ${currentAcount2.balance}`);
 
-currentAcount1.withdraw(1000);
+currentAcount1.withdraw(500);
 currentAcount2.withdraw(1000);
 
 currentAcount1.transfer(1000, currentAcount2);
